7e4c4e10321c70b21837fe21b9b4ef20c9b8f9c5
Shawn D'silva <shawn.dsilva_97@outlook.com>
2025-06-05 19:48:36 +0530

    subscribe and user pic from nav hidden when opening mobile nav

--- script.js
+++ script.js
@@ -14,11 +14,16 @@ document.addEventListener("scroll", () => {
 function toggleMobileNav() {
   const links = document.getElementById("links");
   const mobileNavElem = document.getElementById("mobilenav");
-
+  const subscribe = document.getElementById("mobile-subscribe");
+  const user = document.getElementById("mobile-user");
   if (links.style.display === "block") {
     links.style.display = "none";
     mobileNavElem.style.height = "9%";
+    subscribe.style.display = "unset";
+    user.style.display = "unset";
   } else {
+    subscribe.style.display = "none";
+    user.style.display = "none";
     links.style.display = "block";
     mobileNavElem.style.height = "100%";
   }
